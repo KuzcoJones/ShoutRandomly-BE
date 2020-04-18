f564363732ee5822ab21c4972e1f0a0328f9704f
learn <github email address>
2020-04-18 15:49:15 -0700

    User controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -18,6 +18,13 @@ class AuthController < ApplicationController
         decoded_token = JWT.decode(token, 'secret', true, algorithm: 'HS256')
         user_id = decoded_token[0]['user_id']
         user = User.find(user_id)
+        if user 
+            render json: user.to_json(
+                only: [:id, :bio, :username, :imgUrl], include: [followed_users: {only: [:id, :username, :imgUrl, :bio]},
+                followed_shouts:{only:[:user_id, :body, :likeCount, :commentCount]}])
+        else
+            render json: {error: 'Invalid Token'}, status: 401
+        end
     end
     
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,28 @@
 class UsersController < ApplicationController
-
+    wrap_parameters :user, include: [:username, :password, :bio, :first_name, :last_name, :imgUrl]
     def create 
-        byebug
-        user.find(user_params)
-        # return a new user, token and a message
+        user = User.create!(user_params)
+        # byebug
+        payload = { user_id: user.id }
+        token = JWT.encode(payload, 'secret', 'HS256')
+        # return a message
+        render json: { user: user, token: token}
     end
     
+    
     def index
-        # return a list of all the Users that don't follow the current_user and profiles?
-        byebug
+        token = request.headers[:Authorization].split(' ')[1]
+        decoded_token = JWT.decode(token, 'secret', true, { algorithm: 'HS256'})
+        user_id = decoded_token[0]['user_id']
+        user = User.find(user_id)
+        followed_users_ids = user.followed_users.map{|follow| follow.id }
+        all_users = User.all.reject {|user| followed_users_ids.include? user.id }
+        
+        render json: all_users.to_json(
+            only: [:username, :id, :imgUrl, :bio], include:[followed_users: {only: [:username, :imgUrl, :bio]}]
+            )
     end
+
 
 
     def show
@@ -22,4 +35,16 @@ class UsersController < ApplicationController
         byebug
     end
 
+    private
+    def user_params
+        params.require(:user).permit(:username, :password, :first_name, :bio, :last_name, :imgUrl)
+
+    end
+
 end
+
+# t.string "username"
+#     t.string "password_digest"
+#     t.string "first_name"
+#     t.string "last_name"
+#     t.string "imgUrl"
